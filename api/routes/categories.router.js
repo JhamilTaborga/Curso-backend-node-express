20bc981a4456673f9df7911251d7f7cc30602b31
Jhamil Taborga <jhamiltaborga@gmail.com>
2026-04-15 11:27:16 -0400

    Agregamos el token al usuario y la forma de autenticación con jwt, también protegimos una ruta con jwt

--- api/routes/categories.router.js
+++ api/routes/categories.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 
 const CategoryService = require('./../services/categorie.service');
 const validatorHandler = require('./../middlewares/validator.handler');
@@ -29,6 +30,7 @@ router.get('/:id',
 });
 
 router.post('/',
+  passport.authenticate('jwt', {session: false}),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
