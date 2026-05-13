f59019984d583a124b4deedcb54417bd4a8f5a36
Jhamil Taborga <jhamiltaborga@gmail.com>
2026-05-13 18:41:19 -0400

    Quitamos lineas innecesarias en la seed, usamos variables de entorno para guardar un usuario administrador por defecto, con el password ya hasheado

--- api/db/seeders/20260511174415-first-user-admin.js
+++ api/db/seeders/20260511174415-first-user-admin.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const passport = require('passport');
 const { config } = require('../../config/config');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
    return  queryInterface.bulkInsert('users', [
@@ -11,7 +9,7 @@ module.exports = {
       email: config.gmail,
       password: config.password,
       role: config.role,
-      created_at: new Date()
+      create_at: new Date()
     }
   ]);
   },
